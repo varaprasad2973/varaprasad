228868dd47f8301eb02ef2bee31333469b37574b
varaprasad <varaprasad.2973@gmail.com>
2026-02-24 13:29:59 +0530

    Added App files for the website

---
+++ src/test/java/org/example/Apptest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Apptest {
+    public void testAdd(){
+         App app = new App();
+         int result = app.add(2,3);
+         System.out.println("Running test: 2 = 3 = " + result);
+
+         Assert.assertEquals(5,result);
+    }
+}
